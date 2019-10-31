2b3c9ca5cc12fd36aa4e89ff6edc6c4d73ff9e7c
woodtop <cwood89@mac.com>
2019-10-30 20:46:03 -0400

    created post component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from "./components/post"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Posts />
         <a
           className="App-link"
           href="https://reactjs.org"
