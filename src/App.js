d1315dae8872caba5eaf6bfd81511c0e7726c582
woodtop <cwood89@mac.com>
2019-11-04 23:42:03 -0500

    implemented fetch api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Posts from "./components/post"
+import PostForm from './components/postForm';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <PostForm />
         <Posts />
         <a
           className="App-link"
