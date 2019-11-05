341930cd807538a332948631ae3c2cf63fd61bf2
woodtop <cwood89@mac.com>
2019-11-05 02:03:20 -0500

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from "./components/post"
 import PostForm from './components/postForm';
 
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <PostForm />
-        <Posts />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <PostForm />
+          <Posts />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
